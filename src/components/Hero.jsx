5625d8b451d8e34eb52c86ff6562ef916164d6c1
madanydev21 <email_madanydev21@gmail.com>
2025-12-03 21:26:52 +0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (
         <section style={{ padding: "80px 20px", textAlign: "center" }}>
             <h1 style={{ fontSize: "48px", marginBottom: "10px" }}>Madany Dev</h1>
-            <p style={{ fontSize: "20px" }}>Backend & Fullstack Developer</p>
+            <p style={{ fontSize: "20px" }}>Fullstack Developer</p>
         </section>
     );
 }
